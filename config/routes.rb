ee23ebfc2a940eba93ac6d01b844e1c5b5d72cd5
Harrison Chen <harrisonxchen@gmail.com>
2015-01-31 12:41:44 -0800

    add users

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
       resources :opengov
     end
   end
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
